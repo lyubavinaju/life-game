8c9722055223174011762fc3ff3ff626db266128
lyubavinaju <lyubavinaju@mail.ru>
2026-02-19 12:02:14 +0200

    Refactoring of algorithm

--- src/services/LifeGameService.ts
+++ src/services/LifeGameService.ts
@@ -5,44 +5,32 @@ export default class LifeGameService {
         return this._matrix;
     }
     nextMatrix() {
-        const nextMatrix = Array.from({ length: this._matrix.length },
-            () => Array.from({ length: this._matrix[0].length }, () => 0));
-        for (let rowId = 0; rowId < this._matrix.length; rowId++) {
-            for (let columnId = 0; columnId < this._matrix[0].length; columnId++) {
-                const countAlive = this.countNeighborAlive(rowId, columnId);
-                if (this.isAlive(rowId, columnId) && (countAlive === 2 || countAlive === 3)
-                    || !this.isAlive(rowId, columnId) && countAlive === 3) {
-                    nextMatrix[rowId][columnId] = 1;
-                } else {
-                    nextMatrix[rowId][columnId] = 0;
-                }
-            }
-        }
-        this._matrix = nextMatrix;
+        this._matrix = this._matrix.map((row, rowInd) => this.newRow(row, rowInd));
         return this._matrix;
     }
-
-    private isAlive(rowId: number, columnId: number): boolean {
-        return this._matrix[rowId][columnId] === 1;
+    private newRow(row: number[], rowInd: number): number[] {
+        return row.map((cell, columnInd) => this.newCell(cell, rowInd, columnInd));
     }
-
-    private countNeighborAlive(rowId: number, columnId: number): number {
-        let countAlive = 0;
-        for (let rowStep = -1; rowStep <= 1; rowStep++) {
-            for (let columnStep = -1; columnStep <= 1; columnStep++) {
-                const neighborRowId = rowId + rowStep;
-                const neighborColumnId = columnId + columnStep;
-                if ((neighborRowId === rowId && neighborColumnId === columnId)
-                    || (neighborRowId < 0 || neighborRowId >= this._matrix.length)
-                    || (neighborColumnId < 0 || neighborColumnId >= this._matrix[0].length)
-                ) {
-                    continue;
-                }
-                if (this.isAlive(neighborRowId, neighborColumnId)) {
-                    countAlive++;
-                }
-            }
-        }
-        return countAlive;
+    private newCell(cell: number, rowInd: number, columnInd: number): number {
+        const nNeighbors: number = this.getCountNeighbors(cell, rowInd, columnInd);
+        return cell ? forLiveCell(nNeighbors) : forDeadCell(nNeighbors);
     }
+    private getCountNeighbors(cell: number, rowInd: number, columnInd: number): number {
+        const bounderMatrix: number[][] = this.getBounderMatrix(rowInd, columnInd);
+        const count = bounderMatrix.flatMap(row => row).reduce((acc, cell) => acc + cell);
+        return count - cell;
+    }
+    private getBounderMatrix(rowInd: number, columnInd: number): number[][] {
+        const start = columnInd === 0 ? 0 : columnInd - 1;
+        const end = columnInd === this._matrix[0].length - 1 ? columnInd + 1 : columnInd + 2;
+        return [rowInd - 1, rowInd, rowInd + 1].map(ind => this._matrix[ind]? this._matrix[ind].slice(start, end) : [0]);
+    }
+}
+
+function forLiveCell(nNeighbors: number): number {
+    return +(nNeighbors === 2 || nNeighbors === 3);
+}
+
+function forDeadCell(nNeighbors: number): number {
+    return +(nNeighbors === 3);
 }
